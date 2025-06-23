5b1127040bdf47bf6e8ba84b38e2aae1139e449e
Kgangmin <okm3211@naver.com>
2025-06-23 18:11:07 +0900

    이미지 + 메인페이지 목록추가

--- semiproject/src/main/java/myshop/domain/StayVO.java
+++ semiproject/src/main/java/myshop/domain/StayVO.java
@@ -16,4 +16,122 @@ public class StayVO {
     private String address;					// 주소
     private String detailaddres;			// 상세주소
     private String extraaddress;			// 참고항목
+    
+    
+	public String getStay_no() {
+		return stay_no;
+	}
+	
+	public void setStay_no(String stay_no) {
+		this.stay_no = stay_no;
+	}
+	
+	public String getStay_name() {
+		return stay_name;
+	}
+	
+	public void setStay_name(String stay_name) {
+		this.stay_name = stay_name;
+	}
+	
+	public String getFk_stay_category_no() {
+		return fk_stay_category_no;
+	}
+	
+	public void setFk_stay_category_no(String fk_stay_category_no) {
+		this.fk_stay_category_no = fk_stay_category_no;
+	}
+	
+	public String getStay_thumbnail() {
+		return stay_thumbnail;
+	}
+	
+	public void setStay_thumbnail(String stay_thumbnail) {
+		this.stay_thumbnail = stay_thumbnail;
+	}
+	
+	public String getStay_info() {
+		return stay_info;
+	}
+	
+	public void setStay_info(String stay_info) {
+		this.stay_info = stay_info;
+	}
+	
+	public String getStay_tel() {
+		return stay_tel;
+	}
+	
+	public void setStay_tel(String stay_tel) {
+		this.stay_tel = stay_tel;
+	}
+	
+	public int getLatitude() {
+		return latitude;
+	}
+	
+	public void setLatitude(int latitude) {
+		this.latitude = latitude;
+	}
+	
+	public int getLongitude() {
+		return longitude;
+	}
+	
+	public void setLongitude(int longitude) {
+		this.longitude = longitude;
+	}
+	
+	public int getStay_score() {
+		return stay_score;
+	}
+	
+	public void setStay_score(int stay_score) {
+		this.stay_score = stay_score;
+	}
+	
+	public int getViews() {
+		return views;
+	}
+	
+	public void setViews(int views) {
+		this.views = views;
+	}
+	
+	public String getPostcode() {
+		return postcode;
+	}
+	
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getDetailaddres() {
+		return detailaddres;
+	}
+	
+	public void setDetailaddres(String detailaddres) {
+		this.detailaddres = detailaddres;
+	}
+	
+	public String getExtraaddress() {
+		return extraaddress;
+	}
+	
+	public void setExtraaddress(String extraaddress) {
+		this.extraaddress = extraaddress;
+	}
+	
+    
+    
+    
+    
 }
