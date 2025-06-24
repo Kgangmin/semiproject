5f335f6a27d8c9677fff32fd843cd35fcceaf351
82102 <82102@DESKTOP-8J1OAVB>
2025-06-24 15:06:41 +0900

    회원가입수정

--- semiproject/src/main/java/member/model/MemberDAO.java
+++ semiproject/src/main/java/member/model/MemberDAO.java
@@ -8,10 +8,16 @@ import member.domain.MemberVO;
 
 public interface MemberDAO {
 
-	
+	// ID 중복검사 (tbl_member 테이블에서 userid 가 존재하면 true 를 리턴해주고, userid 가 존재하지 않으면 false 를 리턴한다) 
+		boolean idDuplicateCheck(String userid) throws SQLException;
 
 	// 이메일 중복검사 (tbl_member 테이블에서 email 이 존재하면 true 를 리턴해주고, email 이 존재하지 않으면 false 를 리턴한다)  
 		boolean emailDuplicateCheck(String email) throws SQLException;
+
+	// 회원가입을 해주는 메소드 (tbl_member 테이블에 insert)
+	int registerMember(MemberVO member) throws SQLException;
+
+	
 
 	
 	
--- semiproject/src/main/java/member/model/MemberDAO_imple.java
+++ semiproject/src/main/java/member/model/MemberDAO_imple.java
@@ -59,6 +59,35 @@ public class MemberDAO_imple implements MemberDAO {
 		}
 	}// end of private void close()---------------
 
+	// ID 중복검사 (tbl_member 테이블에서 userid 가 존재하면 true 를 리턴해주고, userid 가 존재하지 않으면 false 를 리턴한다) 
+		@Override
+		public boolean idDuplicateCheck(String userid) throws SQLException {
+			
+			boolean isExists = false;
+			
+			try {
+				  conn = ds.getConnection();
+				  
+				  String sql = " select user_id "
+				  		     + " from tbl_user "
+				  		     + " where user_id = ? ";
+				  
+				  pstmt = conn.prepareStatement(sql);
+				  pstmt.setString(1, userid);
+				  
+				  rs = pstmt.executeQuery();
+				  
+				  isExists = rs.next(); // 행이 있으면 true  (중복된 userid) 
+				                        // 행이 없으면 false (사용가능한 userid) 
+				
+			} finally {
+				close();
+			}
+			
+			return isExists;
+		}// end of public boolean idDuplicateCheck(String userid) throws SQLException------
+		
+		
 	// 이메일 중복검사 (tbl_member 테이블에서 email 이 존재하면 true 를 리턴해주고, email 이 존재하지 않으면 false 를 리턴한다) 
 		@Override
 		public boolean emailDuplicateCheck(String email) throws SQLException {
@@ -69,7 +98,7 @@ public class MemberDAO_imple implements MemberDAO {
 				  conn = ds.getConnection();
 				  
 				  String sql = " select email "
-				  		     + " from tbl_member "
+				  		     + " from tbl_user "
 				  		     + " where email = ? ";
 				  
 				  pstmt = conn.prepareStatement(sql);
@@ -89,7 +118,39 @@ public class MemberDAO_imple implements MemberDAO {
 			return isExists;		
 		}// end of public boolean emailDuplicateCheck(String email) throws SQLException-------
 
-
+		// 회원가입을 해주는 메소드 (tbl_member 테이블에 insert)
+				@Override
+				public int registerMember(MemberVO member) throws SQLException {
+					
+					int result = 0;
+					
+					try {
+						  conn = ds.getConnection();
+						 
+						  String sql = " insert into tbl_user(user_name, user_id, user_pwd, mobile, email, birthday, fk_grade_no) " 
+						  		     + " values(?, ?, ?, ?, ?, to_date(?, 'yyyy-mm-dd'), ? )";
+						  
+						  pstmt = conn.prepareStatement(sql);
+						  
+						  pstmt.setString(1, member.getUser_name());
+						  pstmt.setString(2, member.getUser_id());
+						  pstmt.setString(3, Sha256.encrypt(member.getUser_pwd()) ); // 암호를 SHA256 알고리즘으로 단방향 암호화 시킨다. 
+						  pstmt.setString(4, aes.encrypt(member.getMobile()) ); // 휴대폰을 AES256 알고리즘으로 양방향 암호화 시킨다.			
+						  pstmt.setString(5, aes.encrypt(member.getEmail()) );
+						  pstmt.setString(6, member.getBirthday());
+						  pstmt.setString(7, "6");
+						  result = pstmt.executeUpdate();
+						  
+					} catch(GeneralSecurityException | UnsupportedEncodingException e) {
+						  e.printStackTrace();
+					} finally {
+						  close();
+					}
+					
+					return result;
+					
+				}// end of public int registerMember(MemberVO member) throws SQLException-----------
+				
 	
 	
 	
